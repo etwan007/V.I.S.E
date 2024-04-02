755a3ae301200449787e0441c6d22edcfb9b931e
Ethan Fuller <etwan007@gmail.com>
2024-04-02 00:12:31 -0400

    Steppers done, main still has stepper code which will be changed. PNG uploaded

--- VISE/src/main.cpp
+++ VISE/src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
+
+// Define Constants
+ 
+// Connections to A4988
+const int dirPin = 48;  // Direction
+const int stepPin = 46; // Step
+int en = A8;
+// Motor steps per rotation
+const int STEPS_PER_REV = 2038;
+ 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(en, OUTPUT);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
+  // Set motor direction clockwise
+  digitalWrite(dirPin,HIGH); 
+  digitalWrite(en, LOW);
+  
+  // Spin motor one rotation slowly
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(2000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(2000); 
+  }
+  
+  // Pause for one second
+  delay(1000); 
+  
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin,LOW);
+  
+  // Spin motor two rotations quickly
+  for(int x = 0; x < (STEPS_PER_REV * 2); x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(1000);
+  }
+  
+  // Pause for one second
+  delay(1000);
 }
